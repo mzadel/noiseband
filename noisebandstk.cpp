c0d2b0e70805305adea6d1166f5acac5bcf851a0
Mark Zadel <mark@web008065.wireless.mcgill.ca>
2010-03-25 19:23:06 -0400

    filled in the options table

--- noisebandstk.cpp
+++ noisebandstk.cpp
@@ -32,15 +32,18 @@ enum { WHITENOISE = 0, PINKNOISE };
 
 static struct option long_options[] =
 {
-    /* These options set a flag. */
-/*  {"verbose", no_argument,       &verbose_flag, 1}, */
-/*  {"brief",   no_argument,       &verbose_flag, 0}, */
-    /* These options don't set a flag.  We distinguish them by their indices. */
-    {"add",     no_argument,       0, 'a'},
-    {"append",  no_argument,       0, 'b'},
-    {"delete",  required_argument, 0, 'd'},
-    {"create",  required_argument, 0, 'c'},
-    {"file",    required_argument, 0, 'f'},
+    {"add",         no_argument,       0, 'a'},
+    {"append",      no_argument,       0, 'b'},
+    {"delete",      required_argument, 0, 'd'},
+    {"create",      required_argument, 0, 'c'},
+    {"file",        required_argument, 0, 'f'},
+    {"length",      required_argument, 0, 'l'},
+    {"startfreq",   required_argument, 0, 's'},
+    {"endfreq",     required_argument, 0, 'e'},
+    {"ppo",         required_argument, 0, 'p'}, // partials per octave
+    {"numpartials", required_argument, 0, 'n'}, // total number of partials
+    {"white",       no_argument,       0, 'w'},
+    {"pink",        no_argument,       0, 'p'},
     {0, 0, 0, 0}
 };
 
